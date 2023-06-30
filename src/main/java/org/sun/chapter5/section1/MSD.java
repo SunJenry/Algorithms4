2432e6fbe1f6947cd6a01e33aeb35ec7e28db6f8
sunguangjie <sunguangjie@bilibili.com>
2023-06-30 10:26:28 +0800

    1.chapter 5，update msd sorting.

--- src/main/java/org/sun/chapter5/section1/MSD.java
+++ src/main/java/org/sun/chapter5/section1/MSD.java
@@ -2,6 +2,8 @@ package org.sun.chapter5.section1;
 
 import org.sun.chapter2.section1.InsertionSort;
 
+import static org.sun.util.Util.exch;
+
 public class MSD {
 
     private static int R = 256;//radix
@@ -16,7 +18,8 @@ public class MSD {
 
     private static void sort(String[] a, int lo, int hi, int d) {
         if (hi <= lo + M) {
-//            Insertion.sort(a, lo, hi, d); return;
+            insertionSort(a, lo, hi, d);
+            return;
         }
 
         int[] count = new int[R + 2];
@@ -39,6 +42,18 @@ public class MSD {
         for (int i = 0; i < R; i++) {
             sort(a, lo + count[i], lo + count[i + 1] - 1, d + 1);
         }
+    }
+
+    private static void insertionSort(String[] a, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1], d); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+    private static boolean less(String v, String w, int d) {
+        return v.substring(d).compareTo(w.substring(d)) < 0;
     }
 
     private static int charAt(String s, int d) {
